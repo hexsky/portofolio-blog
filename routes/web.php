9f33a058bd38c6a065d23684ead9a72f0f7de85d
Achmad Fitto Rizky <noo2nolife@gmail.com>
2025-07-05 17:16:15 +0700

    feat: Define routes for public pages and admin CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
 
+use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Halaman Publik
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome'); }); // Nanti diubah jadi controller
+Route::get('/portofolio', [PortfolioController::class, 'indexPublic'])->name('portfolio.public');
+Route::get('/blog', [PostController::class, 'indexPublic'])->name('blog.public');
+Route::get('/blog/{post:slug}', [PostController::class, 'showPublic'])->name('blog.show');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+// Halaman Admin yang dilindungi Auth
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Menggunakan Route Resource untuk CRUD otomatis
+    Route::resource('dashboard/portfolios', PortfolioController::class)->except(['show']);
+    Route::resource('dashboard/posts', PostController::class)->except(['show']);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
